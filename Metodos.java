ea5bd2426fdfcb22844566cff0955828cc21863b
Feitosa Junior <juniorfeito2016@gmail.com>
2023-07-07 11:39:15 -0300

    Adds switch cases

--- Metodos.java
+++ Metodos.java
@@ -1,5 +1,3 @@
-import java.security.DrbgParameters.Reseed;
-
 import javax.swing.*; 
 
 public class Metodos {
@@ -37,25 +35,61 @@ public class Metodos {
 // Declaração de variaveis.
         int n1, n2;
         double r;
+        char op = '0';
         String msg;
+
+        String msgOp = "Escolha opção:";
+        msgOp = msgOp +"\n1 - Para somar os números: ";
+        msgOp = msgOp +"\n2 - Para subtração: ";
+        msgOp = msgOp +"\n3 - Para produto: ";
+        msgOp = msgOp +"\n4 - Para divisão: ";
+        msgOp = msgOp +"\n5 - Para sair: ";
 
 // Entrada de dados 
         n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero:"));
+        op = (JOptionPane.showInputDialog(msgOp)).charAt(0);
 
 // Processamento
-        r = soma(n1,n2);
-        msg = "A soma de " + n1 + " e " + n2 + " é: " + r + "\n";
+        switch (op){
+            case '1': {
+                r = soma(n1,n2);
+                msg = "A soma de " + n1 + " e " + n2 + " é: " + r + "\n";
+                JOptionPane.showMessageDialog(null, msg);
+                System.out.println("\n" + msg + "\n");
+                break;
+            }
 
-        r = diferenca(n1,n2);
-        msg = msg + "\nA diferença de " + n1 + " e " + n2 + " é: " + r + "\n";
-        
-        r = produto(n1,n2);
-        msg = msg + "\nO produto de " + n1 + " e " + n2 + " é: " + r + "\n";
-        
-        r = quociente(n1,n2);
-        msg = msg + "\nO quociente de " + n1 + " e " + n2 + " é: " + r;
-       
-        JOptionPane.showMessageDialog(null, msg);
+            case '2': {
+                r = diferenca(n1,n2);
+                msg =  "A diferença de " + n1 + " e " + n2 + " é: " + r + "\n";
+                JOptionPane.showMessageDialog(null, msg);
+                System.out.println("\n" + msg + "\n");
+                break;
+
+            }
+
+            case '3': {
+                r = produto(n1,n2);
+                msg = "O produto de " + n1 + " e " + n2 + " é: " + r + "\n";
+                JOptionPane.showMessageDialog(null, msg);
+                System.out.println("\n" + msg + "\n");
+                break;
+            }
+
+            case '4': {
+                r = quociente(n1,n2);
+                msg = "O quociente de " + n1 + " e " + n2 + " é: " + r;
+                JOptionPane.showMessageDialog(null, msg);
+                System.out.println("\n" + msg + "\n");
+                break;
+            }
+
+            case '5': {
+                JOptionPane.showMessageDialog(null, "Obrigado por usar o programa !");
+                break;
+            }
+            default: JOptionPane.showMessageDialog(null, "Opção invalida, tente novamente.");
+        }
     } 
 }
